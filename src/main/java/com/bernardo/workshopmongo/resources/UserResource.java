800fc015dc59e649c466ad39789cc0fb1d466171
BernardoFilippi <kirfe06@gmail.com>
2023-09-06 17:40:15 -0300

    Endpoint User posts

--- src/main/java/com/bernardo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/bernardo/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.bernardo.workshopmongo.domain.Post;
 import com.bernardo.workshopmongo.domain.User;
 import com.bernardo.workshopmongo.dto.UserDTO;
 import com.bernardo.workshopmongo.services.UserService;
@@ -57,5 +58,10 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 
+	}
+	@RequestMapping(value="/{id}/posts",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
